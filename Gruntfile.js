cf7c447ca0198b266180229057f1d9adacd90060
Sergio Shapoval <sergiishapoval@googlemail.com>
2016-03-30 07:17:22 +0300

    Revert "Wufoo integration prepared"

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,9 +15,6 @@ module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
-  // Logging grunt output and task information to a log file
-  require ('logfile-grunt')(grunt);
-
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
@@ -151,10 +148,7 @@ module.exports = function (grunt) {
           ]
         }]
       },
-      server: '.tmp',
-      css: {
-         src: [ 'styles/' ]
-      }
+      server: '.tmp'
     },
 
     // Add vendor prefixed styles
@@ -403,24 +397,6 @@ module.exports = function (grunt) {
         cwd: '<%= yeoman.app %>/styles',
         dest: '.tmp/styles/',
         src: '{,*/}*.css'
-      },
-      css: {
-         expand: true,
-         cwd: '<%= yeoman.app %>/styles',
-         dest: 'styles/',
-         src: '{,*/}*.css'
-      },
-      newcss: {
-         expand: true,
-         cwd: '<%= yeoman.dist %>/styles',
-         dest: 'styles/',
-         src: '{,*/}*.css'
-      },
-      back: {
-         expand: true,
-         cwd: 'styles/',
-         dest: '<%= yeoman.dist %>/styles',
-         src: 'combined.css'
       }
     },
 
@@ -456,7 +432,7 @@ module.exports = function (grunt) {
       },
       pages: {
         options: {
-          remote: 'https://katana-ua:aaf4f1cc86245c752aff5c47e06322feb913fc06@github.com/katana-ua/nasha_lepta.git',
+          remote: 'https://SergiiShapoval:880682ec0bdf06406ebda4e7148cbf70840a1161@github.com/SergiiShapoval/nasha_lepta.git',
           branch: 'gh-pages'
         }
       }
@@ -547,14 +523,10 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'copy:css',
     'cdnify',
     'cssmin',
-    'copy:newcss',
     'uglify',
     'filerev',
-    'copy:back',
-    'clean:css',
     'usemin',
     'htmlmin'
   ]);
